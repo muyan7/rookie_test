5a0e6409d5b7382187b9520754a6f91cce6aa9d6
flower30 <1418701248@qq.com>
2022-08-29 23:34:38 +0800

    首页静态页面完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//三级联动组件，全局组件
+import TypeNav from '../src/pages/Home/TypeNav';
+// 注册全局组件，第一个参数是名字，第二个参数是哪一个组件
+Vue.component(TypeNav.name,TypeNav)
 Vue.config.productionTip = false
 //引入路由
 import router from '../src/router'
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,10 +1,34 @@
 <template>
-  <div>我是首页</div>
+  <div>
+    <!-- 三级联动组件；已经是全局组件，不需要在用其他方式引入 -->
+    <TypeNav/>
+    <ListContainer/>
+    <Recommend/>
+    <Rank/>
+    <Like/>
+    <Floor/>
+    <Floor/>
+    <Brand/>
+  </div>
 </template>
 
 <script>
+// 引入其余的组件
+// 轮播图列表组件
+import ListContainer from '../../pages/Home/ListContainer'
+// 引入今日推荐系列
+import Recommend from '../../pages/Home/Recommend'
+// 引入排行榜
+import Rank from '../../pages/Home/Rank'
+// 引入猜你喜欢
+import Like from '../../pages/Home/Like'
+// 引入楼层推荐
+import Floor from '../../pages/Home/Floor'
+// 引入商标
+import Brand from '../../pages/Home/Brand'
 export default {
   name: 'Home',
+  components:{ListContainer,Recommend,Rank,Like,Floor,Brand}
 }
 </script>
 
